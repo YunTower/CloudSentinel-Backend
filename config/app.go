37475d0b6fb62ad118c3594147ad0a0b6c9f4cd0
ohhhfish <xyfishcn@qq.com>
2025-11-28 18:58:21 +0800

    🔧 移除不再使用的服务提供者和应用配置项

--- config/app.go
+++ config/app.go
@@ -12,7 +12,8 @@ import (
 	"github.com/goravel/framework/log"
 	"github.com/goravel/framework/queue"
 	"github.com/goravel/framework/route"
-	"github.com/goravel/framework/schedule"
+
+	// "github.com/goravel/framework/schedule"
 	"github.com/goravel/framework/support/carbon"
 	"github.com/goravel/framework/validation"
 	"github.com/goravel/gin"
@@ -52,25 +53,10 @@ func init() {
 		// More: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 		"timezone": carbon.UTC,
 
-		// Application Locale Configuration
-		//
-		// The application locale determines the default locale that will be used
-		// by the translation service provider. You are free to set this value
-		// to any of the locales which will be supported by the application.
-		"locale": "en",
-
-		// Application Fallback Locale
-		//
-		// The fallback locale determines the locale to use when the current one
-		// is not available. You may change the value to correspond to any of
-		// the language folders that are provided through your application.
-		"fallback_locale": "en",
-
-		// Application Lang Path
-		//
-		// The path to the language files for the application. You may change
-		// the path to a different directory if you would like to customize it.
-		"lang_path": "lang",
+		// Application Locale Configuration (not used: translation service provider is disabled)
+		// "locale": "en",
+		// "fallback_locale": "en",
+		// "lang_path": "lang",
 
 		// Encryption Key
 		//
@@ -92,7 +78,7 @@ func init() {
 			&cache.ServiceProvider{},
 			&http.ServiceProvider{},
 			&route.ServiceProvider{},
-			&schedule.ServiceProvider{},
+			//&schedule.ServiceProvider{},
 			//&event.ServiceProvider{},
 			&queue.ServiceProvider{},
 			//&grpc.ServiceProvider{},
